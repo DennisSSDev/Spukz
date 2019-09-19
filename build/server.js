89746745e73ab02346addec5ff85182de528ab3e
Dennis Slavinsky <dennis.slavinsky@gmail.com>
2019-09-19 00:54:30 -0400

    final fix

--- build/server.js
+++ build/server.js
@@ -5,13 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
-app.use(express_1.default.static(__dirname + "/../public"));
+app.use(express_1.default.static(__dirname + "/../client/build"));
 app.get('/api/getList', function (req, res) {
     var list = ['item1', 'item2', 'item3'];
     res.json(list);
 });
 app.get('*', function (req, res) {
-    res.sendFile(__dirname + "/../public/index.html");
+    res.sendFile(__dirname + "/../client/build/index.html");
 });
 var port = process.env.PORT || 5000;
 // console.log that your server is up and running
