5f831b2953f7559db4ed141fa169e113c7dca23b
Dennis Slavinsky <dennis.slavinsky@gmail.com>
2019-09-19 00:16:06 -0400

    test

--- build/server.js
+++ build/server.js
@@ -3,17 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var path_1 = __importDefault(require("path"));
-var app = express_1.default();
+var express_1 = require("express");
+var path_1 = require("path");
+var app = express_1();
 var port = process.env.PORT || 5000;
-app.use(express_1.default.static(path_1.default.join(__dirname, 'client/build')));
+app.use(express_1.static(path_1.join(__dirname, 'client/build')));
 app.get('/api/getList', function (req, res) {
     var list = ['item1', 'item2', 'item3'];
     res.json(list);
 });
 app.get('*', function (req, res) {
-    res.sendFile(path_1.default.join(__dirname + "/client/build/index.html"));
+    res.sendFile(path_1.join(__dirname + "/client/build/index.html"));
 });
 // console.log that your server is up and running
 app.listen(port, function () { return console.log("Listening on port " + port); });
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,8 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-process.env.PWD = process.cwd();
-
-app.use(express.static(path.join(process.env.PWD, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/api/getList', (req, res) => {
   const list = ['item1', 'item2', 'item3'];
@@ -14,8 +11,9 @@ app.get('/api/getList', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(process.env.PWD, 'client/build/index.html'));
+  res.sendFile(`${__dirname}/client/build/index.html`);
 });
 
+const port = process.env.PORT || 5000;
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
