912fcbbfade414fedf090406899af802e17eb9ec
Dennis Slavinsky <dennis.slavinsky@gmail.com>
2019-09-19 00:05:58 -0400

    test

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ import path from 'path';
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+process.env.PWD = process.cwd();
+
+app.use(express.static(path.join(process.env.PWD, 'client/build')));
 
 app.get('/api/getList', (req, res) => {
   const list = ['item1', 'item2', 'item3'];
@@ -12,7 +14,7 @@ app.get('/api/getList', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  res.sendFile(path.join(process.env.PWD, 'client/build/index.html'));
 });
 
 // console.log that your server is up and running
