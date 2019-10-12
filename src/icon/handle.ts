edd3394777dc573b9d50d978a7d43104554b1288
Dennis Slavinsky <dennis.slavinsky@gmail.com>
2019-10-11 20:35:06 -0400

    icon distribution

---
+++ src/icon/handle.ts
@@ -0,0 +1,33 @@
+import { Response, Request } from 'express';
+import handleAccept from '../misc/helper';
+import { Type } from '../global/dataTypes';
+import { QueryIcon } from '../global/dataStore';
+
+const GetIcon = (req: Request, res: Response) => {
+  const { accept } = req.headers;
+
+  if (handleAccept(accept, res)) {
+    return;
+  }
+
+  const { query } = req;
+  const { type } = query;
+  if (!type) {
+    res.status(400).json({
+      id: 'ErrorBadIconRequest',
+      message: 'You must specify the type of icon you require'
+    });
+    return;
+  }
+
+  if (!Object.keys(Type).includes(type)) {
+    res.status(400).json({
+      id: 'ErrorBadIconRequest',
+      message: 'The Icon type you specified does not exist'
+    });
+    return;
+  }
+  res.json(QueryIcon(type));
+};
+
+export default GetIcon;
